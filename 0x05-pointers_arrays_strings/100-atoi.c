c1edae7a313ca83db74705ef415feb47a339cf79
dennnji <njihiandiba94@gmail.com>
2022-12-19 08:20:59 -0800

    100-main.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: Always 0 (Success)
+ * Return: the int converted from the string
  */
-int main(void)
+int _atoi(char *s)
 {
-	int pass[100];
-	int i, sum, n;
+	int i, d, n, len, f, digit;
 
-	sum = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	srand(time(NULL));
+	while (s[len] != '\0')
+		len++;
 
-	for (i = 0; i < 100; i++)
+	while (i < len && f == 0)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
+		i++;
 	}
 
-	return (0);
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
 
--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("%d\n", nb);
 	nb = _atoi("-402");
 	printf("%d\n", nb);
-	nb = _atoi("          ------++++++-----+++++--98");
+	nb = _atoi(" ------++++++-----+++++--98");
 	printf("%d\n", nb);
 	nb = _atoi("214748364");
 	printf("%d\n", nb);
@@ -22,7 +22,7 @@ int main(void)
 	printf("%d\n", nb);
 	nb = _atoi("Suite 402");
 	printf("%d\n", nb);
-	nb = _atoi(" ++ --98 Battery Street; San Francisco, CA 94111 - USA");
+	nb = _atoi(" ++  --98 Battery Street; San Francisco, CA 94111 - USA");
 	printf("%d\n", nb);
 	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
 	printf("%d\n", nb);
