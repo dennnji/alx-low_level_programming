fc4bea56a9722caf607c0d14812c2f42d4d8da27
dennnji <njihiandiba94@gmail.com>
2022-12-21 15:24:27 -0800

    0-main.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ int j;
 i = 0;
 while (dest[i] != '\0')
 {
-++;
+i++;
 }
 j = 0;
 while (src[j] != '\0')
@@ -22,9 +22,10 @@ while (src[j] != '\0')
 dest[i] = src[j];
 i++;
 j++;
-	}
+}
 
 dest[i] = '\0';
 return (dest);
 }
+i
 
