14ff15ce2b988fcb121e1c657ea166ed4256d70c
dennnji <njihiandiba94@gmail.com>
2022-12-30 14:46:29 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>                                             
 #include <time.h>                                               
 #include <stdio.h>                                              
                                                                 
@@ -10,29 +9,29 @@
  */                                                             
 int main(void)                                                  
 {                                                               
-        int n;                                                  
+int n;
                                                                 
-        srand(time(0));                                         
-        n = rand() - RAND_MAX / 2;                              
+srand(time(0));
+n = rand() - RAND_MAX / 2
                                                                 
-        if ((n % 10) > 5)                                       
-        {                                                       
-                printf("Last digit of %d is %d and is greater th
-an 5\n",                                                        
-                        n, n % 10);                             
-        }                                                       
-        else if ((n % 10) < 6 && (n % 10) != 0)                 
-        {                                                       
-                printf("Last digit of %d is %d and is less than 
-6 and not 0\n",                                                 
-                        n, n % 10);                             
-        }                                                       
-        else                                                    
-        {                                                       
-                printf("Last digit of %d is %d and is 0\n",     
-                        n, n % 10);                             
-        }                                                       
+if ((n % 10) > 5
+{
+printf("Last digit of %d is %d and is greater th
+an 5\n",
+n, n % 10);
+}
+else if ((n % 10) < 6 && (n % 10) != 0)
+{
+printf("Last digit of %d is %d and is less than
+6 and not 0\n",
+n, n % 10);
+}
+else
+{
+printf("Last digit of %d is %d and is 0\n",
+n, n % 10);
+}
                                                                 
-        return (0);                                             
+return (0);
                                                                 
-}                                                               
+}
